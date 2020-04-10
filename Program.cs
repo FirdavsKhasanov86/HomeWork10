1eaa1f919af71960b8aa82dd4ebe9ba9b7621d30
user <user@Williams-iMac.local>
2020-04-10 14:25:54 +0500

    Added new derived class ProDocumentWorker

--- Program.cs
+++ Program.cs
@@ -7,28 +7,49 @@ namespace HomeWork10
         static void Main(string[] args)
         {
             DocumentWorker dw = new DocumentWorker();
+            ProDocumentWorker pdw = new ProDocumentWorker(); 
             dw.OpenDocument();
             dw.EditDocument();
+            dw.SaveDocument();
+            pdw.EditDocument();
+            pdw.SaveDocument();
+            
+            
         }
     }
      class DocumentWorker
      {
 
-         public void OpenDocument(){
+         public virtual void OpenDocument(){
+           
            Console.WriteLine("Документ открыт");
          }
 
-         public void EditDocument(){
+         public virtual void EditDocument(){
+             
             Console.WriteLine("Редоктирование документа доступно в версии Про");
          }
 
-         public void SaveDocument(){
+         public virtual void SaveDocument(){
+            
              Console.WriteLine("Сохранение документа доступно в версии Про");
+             
+             Console.WriteLine("------------------------------------------------");
          }
      } 
 
      class ProDocumentWorker:DocumentWorker
      {
+         
+        public override void EditDocument()
+        {
+            
+            Console.WriteLine("Документ отредоктирван");
+        }
 
+        public override void SaveDocument(){
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+           
+        }
      }
 }
