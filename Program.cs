07b43dc17151c9b5735e946a21bfb08caf14e2eb
user <user@Williams-iMac.local>
2020-04-10 12:40:38 +0500

    Added class DocumentWorker

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace HomeWork10
     {
         static void Main(string[] args)
         {
-            
+            DocumentWorker dw = new DocumentWorker();
+            dw.OpenDocument();
+            dw.EditDocument();
         }
     }
+     class DocumentWorker
+     {
+
+         public void OpenDocument(){
+           Console.WriteLine("Документ открыт");
+         }
+
+         public void EditDocument(){
+            Console.WriteLine("Редоктирование документа доступно в версии Про");
+         }
+
+         public void SaveDocument(){
+             Console.WriteLine("Сохранение документа доступно в версии Про");
+         }
+     } 
+
+     class ProDocumentWorker:DocumentWorker
+     {
+
+     }
 }
